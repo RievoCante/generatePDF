a5792d7b03e50ff831e795452e640847a6124db7
Putter-Ravit <putterbcc@gmail.com>
2024-06-17 10:56:09 +0700

    use html2pdf library

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const PORT = process.env.PORT || 8080;
 const routes = require('./routes/index.js');
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(__dirname + '\\public'));
 app.use(express.json());
-
 app.use('/', routes);
 
 app.listen(PORT, () => console.log(`Server is running on port : ${PORT}`));
